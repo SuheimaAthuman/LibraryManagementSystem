8396befd7e72d52efb6dd970199ace236da0ae1d
SuheimaAthuman-lab <Suheima.athuman@strathmore.edu>
2026-02-24 14:04:41 +0300

    a

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,8 @@
-public class Person {
+public class test {
+    public static void main(String[] args) {
+        Person person = new Person("John", 30);
+        System.out.println(person.getName()); // Output: John
+        System.out.println(person.getAge());  // Output: 30
+    }
     
 }
